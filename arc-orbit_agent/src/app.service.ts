b5634ede394c8534d3b08205fc5540455cc0b3aa
eketeUg <emmanuelugochukwuekete@gmail.com>
2026-05-23 21:07:00 +0000

    docker file

--- arc-orbit_agent/src/app.service.ts
+++ arc-orbit_agent/src/app.service.ts
@@ -21,36 +21,9 @@ dotenv.config();
 export class AppService {
   private client: CircleDeveloperControlledWalletsClient;
 
-  constructor() {
-    this.client = initiateDeveloperControlledWalletsClient({
-      apiKey: process.env.CIRCLE_API_KEY!,
-      entitySecret: process.env.CIRCLE_ENTITY_SECRET!,
-    });
-  }
+  constructor() {}
 
-  async getHello(): Promise<object> {
-    const walletSetResponse = await this.client.createWalletSet({
-      name: 'arcOrbit wallet Set',
-    });
-
-    const walletSet = walletSetResponse.data?.walletSet;
-    if (!walletSet?.id) {
-      throw new Error('Wallet set creation failed: no ID returned');
-    }
-
-    const walletResponse = await this.client.createWallets({
-      walletSetId: walletSet.id,
-      blockchains: ['ARC-TESTNET', 'SOL-DEVNET'], // Can be any supported blockchain
-      count: 1,
-      accountType: 'EOA', // Can be EOA or SCA
-    });
-
-    console.log('Wallet set response:', walletSetResponse.data);
-    console.log('Wallet response:', walletResponse.data);
-    const wallet = walletResponse.data?.wallets;
-    if (!wallet || wallet.length === 0) {
-      throw new Error('Wallet creation failed: no ID returned');
-    }
-    return wallet;
+  async getHello(): Promise<string> {
+    return 'hello world';
   }
 }
